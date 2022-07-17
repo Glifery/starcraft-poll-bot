5ee077f8fa990b33e51553e94ea3cc42e72aad43
Maksim Lysenka <glifery@gmail.com>
2022-07-17 15:45:33 +0300

    Working version

--- src/main/java/com/glifery/starcraft_poll/StarcraftPollHandler.java
+++ src/main/java/com/glifery/starcraft_poll/StarcraftPollHandler.java
@@ -2,13 +2,16 @@ package com.glifery.starcraft_poll;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.springframework.boot.SpringApplication;
 
-public class StarcraftPollHandler implements RequestHandler<String, Object> {
+public class StarcraftPollHandler implements RequestHandler<Object, Object> {
 
     @Override
-    public Object handleRequest(String s, Context context) {
-        context.getLogger().log("Input: " + s);
+    public Object handleRequest(Object s, Context context) {
+        context.getLogger().log("Start lambda with input: " + s);
 
-        return "Lambda Function is invoked....";
+        SpringApplication.run(StarcraftPollApplication.class);
+
+        return "End lambda successfully";
     }
 }
