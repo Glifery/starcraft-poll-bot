f3bf6780a0f503f3ef8570332c297f8747fbfa3d
Maksim Lysenka <glifery@gmail.com>
2022-07-17 13:27:35 +0300

    Some fixes and tries

--- src/main/java/com/glifery/starcraft_poll/StarcraftPollApplication.java
+++ src/main/java/com/glifery/starcraft_poll/StarcraftPollApplication.java
@@ -4,31 +4,42 @@ import com.glifery.starcraft_poll.application.TelegramBot;
 import com.glifery.starcraft_poll.config.App;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cloud.function.context.FunctionRegistration;
+import org.springframework.cloud.function.context.FunctionType;
+import org.springframework.cloud.function.context.FunctionalSpringApplication;
+import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
+import org.springframework.context.support.GenericApplicationContext;
 
 import java.util.function.Function;
 
-@SpringBootApplication
-@RequiredArgsConstructor
-public class StarcraftPollApplication {
+@SpringBootConfiguration
+//@RequiredArgsConstructor
+public class StarcraftPollApplication implements ApplicationContextInitializer<GenericApplicationContext> {
 
-	private final TelegramBot telegramBot;
-	private final App app;
+//	private final TelegramBot telegramBot;
+//	private final App app;
 
 	public static void main(String[] args) {
-		SpringApplication.run(StarcraftPollApplication.class, args);
+		FunctionalSpringApplication.run(StarcraftPollApplication.class, args);
 	}
 
-	@Bean
-	public Function<String, Boolean> containsCloud() {
-		return value -> value.contains("cloud");
+	public Function<String, String> containsCloud() {
+		return value -> "Hello world";
 	}
 
-	@EventListener(ApplicationReadyEvent.class)
-	public void start() {
-		telegramBot.doSendPoll(app.getChatId(), app.getForceSend());
+//	@EventListener(ApplicationReadyEvent.class)
+//	public void start() {
+//		telegramBot.doSendPoll(app.getChatId(), app.getForceSend());
+//	}
+
+	@Override
+	public void initialize(GenericApplicationContext applicationContext) {
+		applicationContext.registerBean("containsCloud", FunctionRegistration.class,
+				() -> new FunctionRegistration<>(containsCloud())
+						.type(FunctionType.from(String.class).to(Boolean.class)));
 	}
 }
