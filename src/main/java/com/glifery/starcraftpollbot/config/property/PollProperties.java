638c7f449ee58335fe64a6a7ff08c88344ceacf3
Maksim Lysenka <glifery@gmail.com>
2022-11-22 11:39:13 +0800

    Pass custom times as env var

--- src/main/java/com/glifery/starcraftpollbot/config/property/PollProperties.java
+++ src/main/java/com/glifery/starcraftpollbot/config/property/PollProperties.java
@@ -2,8 +2,11 @@ package com.glifery.starcraftpollbot.config.property;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
+
+import java.util.List;
 
 @ConfigurationProperties(prefix = "poll")
 @ConstructorBinding
@@ -13,5 +16,7 @@ public class PollProperties {
 
     private final String chatId;
     private final Boolean forceSend;
+    @Value("#{'${times}'.split('|')}")
+    private final List<String> timesList;
 
 }
